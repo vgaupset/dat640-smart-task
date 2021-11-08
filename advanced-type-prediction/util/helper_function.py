ad7abcaf6480a49e0fc4c3b396acfeed018e4279
Lejun Chen <you@example.com>
2021-11-08 20:24:27 +0100

    features 17-19

--- advanced-type-prediction/util/helper_function.py
+++ advanced-type-prediction/util/helper_function.py
@@ -1,6 +1,7 @@
 import json
 import string
 import re
+from typing import Callable, Dict, List, Set, Tuple
 from elasticsearch import Elasticsearch
 
 def load_dict_from_json(filename):
@@ -41,6 +42,70 @@ def preprocess(doc: str) -> str:
     for c in string.punctuation:
         doc = doc.replace(c, " ")
     return doc.lower()
+
+def analyze_query(
+    es: Elasticsearch, query: str, field: str, index: str = "toy_index"
+) -> List[str]:
+    """Analyzes a query with respect to the relevant index.
+
+    Args:
+        es: Elasticsearch object instance.
+        query: String of query terms.
+        field: The field with respect to which the query is analyzed.
+        index: Name of the index with respect to which the query is analyzed.
+
+    Returns:
+        A list of query terms that exist in the specified field among the
+        documents in the index.
+    """
+    tokens = es.indices.analyze(index=index, body={"text": query})["tokens"]
+    query_terms = []
+    for t in sorted(tokens, key=lambda x: x["position"]):
+        # Use a boolean query to find at least one document that contains the
+        # term.
+        hits = (
+            es.search(
+                index=index,
+                query={"match": {field: t["token"]}},
+                _source=False,
+                size=1,
+            )
+            .get("hits", {})
+            .get("hits", {})
+        )
+        doc_id = hits[0]["_id"] if len(hits) > 0 else None
+        if doc_id is None:
+            continue
+        query_terms.append(t["token"])
+    return query_terms
+
+
+def get_doc_term_freqs(
+    es: Elasticsearch, doc_id: str, field: str, index: str
+) -> Dict[str, int]:
+    """Gets the term frequencies of a field of an indexed document.
+
+    Args:
+        es: Elasticsearch object instance.
+        doc_id: Document identifier with which the document is indexed.
+        field: Field of document to consider for term frequencies.
+        index: Name of the index where document is indexed.
+
+    Returns:
+        Dictionary of terms and their respective term frequencies in the field
+        and document.
+    """
+    tv = es.termvectors(
+        index=index, id=doc_id, fields=field, term_statistics=True
+    )
+    if tv["_id"] != doc_id:
+        return None
+    if field not in tv["term_vectors"]:
+        return None
+    term_freqs = {}
+    for term, term_stat in tv["term_vectors"][field]["terms"].items():
+        term_freqs[term] = term_stat["term_freq"]
+    return term_freqs
 
 
 class Indexer:
