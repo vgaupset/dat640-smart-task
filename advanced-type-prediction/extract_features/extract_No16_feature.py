d1b262f937ed57133008f636c8bfcee490165082
Lejun Chen <you@example.com>
2021-11-14 23:12:16 +0100

    --

--- advanced-type-prediction/extract_features/extract_No16_feature.py
+++ advanced-type-prediction/extract_features/extract_No16_feature.py
@@ -17,9 +17,11 @@ FEATURES_16="LENGTH_t"
 # In[ ]:
 
 
-def extract_features_16(data,dp_type:str)-> Dict[str, float]:
-    return {"LENGTH_t": len(data[dp_type])}
+def extract_features_16_variant1(DBpedia_map_type_entities:Dict,dp_type:str)-> Dict[str, float]:
+    return {"LENGTH_t": len(DBpedia_map_type_entities[dp_type])}
 
+def extract_features_16_variant2(dp_type:str)-> Dict[str, float]:
+    return {"LENGTH_t": len(dp_type)}
 
 # In[ ]:
 
@@ -29,5 +31,6 @@ if __name__ == '__main__':
         data = json.load(file)
     #data.keys()
     DBO_type='dbo:Disease'
-    print(extract_features_16(data,DBO_type))
+    print(extract_features_16_variant1(data,DBO_type))
+    print(extract_features_16_variant2(DBO_type))
 
---
+++ advanced-type-prediction/extract_features/extract_No1to10_features.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from elasticsearch import Elasticsearch
+from typing import Dict, List, Optional
+from collections import Counter
+import math,json
+
+
+# In[2]:
+
+
+def extract_features_1to5(DBpedia_map_type_entities:Dict,dp_type:str,query:str,k_list=[5,10,20,50,100],es = Elasticsearch(),index="dbpedia_entity_centric")-> Dict[str, float]:
+    ECBM25_t_q={}
+    for k in k_list:
+        hits = es.search(index=index, q=query, _source=True, size=k )["hits"]["hits"]  
+        score_list= [hit["_score"] for hit in hits]
+        w_e_t=len(DBpedia_map_type_entities[dp_type])      
+        ECBM25_t_q[f'ECBM25_t_q_{k}']=round(sum(score_list)/w_e_t,4)
+    return ECBM25_t_q
+  
+
+
+# In[3]:
+
+if __name__ == '__main__':
+    filepath="../data/DBpedia_map_type_entities.json"
+    with open(filepath, 'r',encoding='utf-8') as f:
+        DBpedia_map_type_entities = json.load(f)
+    print("------length:",len(DBpedia_map_type_entities))
+    dp_type="dbo:Place"
+    question="When was Bibi Andersson married to Per Ahlmark very green?"
+    scores=extract_features_1to5(DBpedia_map_type_entities,dp_type,question)
+    print("------scores:",scores)
+
+
+# In[ ]:
+
+
+
+
