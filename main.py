954ae2b3ab851f6d419ac168b5b9f2c445d340f3
vgaupset <vgaupset@gmail.com>
2021-11-17 19:29:55 +0100

    using type prediction in main function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.insert(1, "./baseline-category-prediction-2")
+sys.path.insert(1, "./advanced-type-prediction")
 sys.path.insert(1, "./smart-dataset/evaluation/dbpedia")
 
 from category_prediction import category_prediction
+from type_prediction import type_prediction
 import evaluate 
 
 
@@ -15,7 +17,8 @@ if __name__ == '__main__':
          "./smart-dataset/datasets/Wikidata/lcquad2_anstype_wikidata_train.json"],
         [path_to_test]
     )
+    type_prediction("smart-dataset/datasets/DBpedia/smarttask_dbpedia_train.json","./category_results.json","./smart-dataset/evaluation/dbpedia/dbpedia_types.tsv", "./results.json")
     type_hierarchy, max_depth = evaluate.load_type_hierarchy('smart-dataset\evaluation\dbpedia\dbpedia_types.tsv')
     ground_truth = evaluate.load_ground_truth(path_to_test, type_hierarchy)
-    system_output = evaluate.load_system_output('./category_results.json')
+    system_output = evaluate.load_system_output('./results.json')
     evaluate.evaluate(system_output, ground_truth, type_hierarchy, max_depth)
