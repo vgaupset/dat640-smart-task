e6fb463cd657049ce8440eaba007bc2789dd6725
vgaupset <vgaupset@gmail.com>
2021-11-20 20:17:01 +0100

    update main and type prediction

--- advanced-type-prediction/type_prediction.py
+++ advanced-type-prediction/type_prediction.py
@@ -2,15 +2,15 @@ import sys, json, csv
 import types
 from typing import Callable, Dict, List, Set, Tuple
 from sklearn.ensemble import RandomForestRegressor
-# import gensim.downloader as api
-# import gensim
+import gensim.downloader as api
+import gensim
 sys.path.insert(1, './advanced-type-prediction/extract_features')
-# from extract_No13to15_features import TypeTaxonomy, extract_features_13to15
-# from extract_No16_feature import extract_features_16
-# from extract_No17to19_features import extract_features_17to19
-# from extract_No20to22_features import extract_features_20to22
-# #from extract_No23to25_features import extract_features_23to25
-# from extract_No23to25_features_optimized import extract_features_23to25
+from extract_No13to15_features import TypeTaxonomy, extract_features_13to15
+from extract_No16_feature import extract_features_16
+from extract_No17to19_features import extract_features_17to19
+from extract_No20to22_features import extract_features_20to22
+#from extract_No23to25_features import extract_features_23to25
+from extract_No23to25_features_optimized import extract_features_23to25
 
 
 def load_data(path):
@@ -28,18 +28,53 @@ def load_types(path):
 
     return types
 
-def extract_features(question, type):
-    # ft13to15 = extract_features_13to15(question, type)
-    # ft16 = extract_features_16(question, type)
-    # ft17to19 = extract_features_17to19(question, type)
-    # ft20to22 = extract_features_20to22(question, type)
-    # ft23to25 = extract_features_23to25(question, type)
-    # features = {**ft13to15, **ft16, **ft17to19, **ft20to22, **ft23to25}.values()
-    features =[0]*10
-    if type == "dbo:Place" and "place" in question:
-        features[1] = 1.2
+def extract_features(
+    question:str,
+    dp_type: str,
+    DBpedia_map_type_entities:Dict,
+    docID_DBOtype_dict:Dict,
+    typeobj:TypeTaxonomy,
+    training_map_type_questions:Dict,
+    model_loaded:gensim.models.keyedvectors.KeyedVectors,
+    es: Elasticsearch,
+    add_extra_features:bool=False
+) -> List[float]:
+    """Extracts features of a query and document pair.
 
-    return features
+        Args:
+            query: string.
+            dp_type: DBO type.
+            es: Elasticsearch object instance.
+
+        Returns:
+            List of extracted feature values in a fixed order.
+    """
+    features_1to5 = extract_features_1to5(DBpedia_map_type_entities,dp_type,question,es)
+    feature_vect = list(features_1to5.values())
+
+    features_11to12 = extract_features_11to12(dp_type, question,docID_DBOtype_dict,es)
+    feature_vect.extend(list(features_11to12.values()))
+
+    features_13to15 = extract_features_13to15(typeobj,dp_type)
+    feature_vect.extend(list(features_13to15.values()))
+    
+    features_16=extract_features_16(DBpedia_map_type_entities,dp_type)
+    feature_vect.extend(list(features_16.values()))
+    
+    features_17to19=extract_features_17to19(training_map_type_questions,dp_type,question)
+    feature_vect.extend(list(features_17to19.values()))
+    
+    features_20to22=extract_features_20to22(dp_type,question)
+    feature_vect.extend(list(features_20to22.values()))
+    
+    features_23to25=extract_features_23to25(model_loaded,dp_type, question, mode="Euclidean")
+    feature_vect.extend(list(features_23to25.values()))
+
+    if add_extra_features:
+        features_23to25_variant=extract_features_23to25(model_loaded,dp_type, question, mode="similarities")
+        feature_vect.extend(list(features_23to25_variant.values()))
+    
+    return feature_vect
 
 def train(X, y):
     return RandomForestRegressor().fit(X,y)
